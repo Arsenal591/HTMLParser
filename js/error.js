16c6b9ced9862b2c6999c61b65de7bba1edd61c3
Arsenal591 <arsenal591@126.com>
2017-07-20 19:41:31 +0800

    add visualization

--- js/error.js
+++ js/error.js
@@ -1,5 +1,6 @@
 class PraseError extends Error{
 	constructor(level, message, rowNumber){
+		this.name = "PraseError";
 		this.level = level;
 		this.message = message;
 		this.lineNumber = lineNumber;
---
+++ js/main.js
@@ -0,0 +1,47 @@
+var inputElement = document.getElementById("input");
+var htmlShow = document.getElementById("htmlshow");
+var errorShow = document.getElementById("errorshow");
+var codeArea = document.getElementById("codeedit");
+var runCodeButton = document.getElementById("runcode");
+var tree;
+
+htmlShow.setAttribute("readonly", true);
+var handleFiles = function() {
+	var fileList = this.files;
+	var file = fileList[0];
+	var reader = new FileReader();
+	reader.onload = function(e) {
+		tree = new DOMTree();
+		var machine = new TokenizeMachine();
+		for (let ch of reader.result) {
+			machine.transfer(ch);
+		}
+	}
+	reader.readAsText(file);
+}
+inputElement.addEventListener("change", handleFiles, false);
+
+
+function addErrorMessage(e){
+	var msg = document.createElement("p");
+	var textNode = document.createTextNode("error");
+	msg.appendChild(textNode);
+	msg.style.backgroundColor = "yellow";
+	errorShow.appendChild(msg);
+}
+
+
+function runCode(){
+	var src = codeArea.value;
+	try{
+		var result = eval(src);
+		console.log(result);
+	}
+	catch(e){
+		addErrorMessage(e);
+	}
+	finally{
+		codeArea.value = '';
+	}
+}
+runCodeButton.addEventListener("click", runCode);
