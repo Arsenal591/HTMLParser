4dfdf305c6fe217c5cdf10ea1e9292047451adf9
Arsenal591 <arsenal591@126.com>
2017-07-21 10:04:20 +0800

    add ugly animation

--- js/visualization.js
+++ js/visualization.js
@@ -1,4 +1,4 @@
-var svg = d3.select("#visualize").append("g").attr("transform", "translate(0,0)");
+var svg = d3.select("#visualize").append("g").attr("transform", "translate(0,20)");
 var svgTree = d3.layout.tree().separation(function(a, b) {
 	return (a.parent === b.parent ? 1 : 2);
 });
@@ -48,7 +48,8 @@ function getTreeNodes(center, level) {
 function redraw(center) {
 	var width = visualization.clientWidth;
 	var height = visualization.clientHeight;
-	var maxLevel = Math.floor(height / 50) + 1;
+	var maxLevel = Math.floor(height / 50);
+	console.log(height, maxLevel);
 
 	var data = getTreeNodes(center, maxLevel);
 
@@ -66,7 +67,7 @@ function redraw(center) {
 	var nodeExit = nodeUpdate.exit();
 
 	var enterNodes = nodeEnter.append("g").attr("class", "node").attr("transform", function(d) {
-		return "translate(" + d.x + "," + d.y + ")";
+		return "translate(" + nodes[0].x + "," + nodes[0].y + ")";
 	})
 	enterNodes.append("circle").attr("r", 5).style("fill", function(d) {
 		return d.isme ? "blue" : "#fff";
@@ -85,6 +86,18 @@ function redraw(center) {
 	});
 
 	var exitNodes = nodeExit.transition().duration(500).attr("transform", function(d) {
-		return "translate(" + d.x + "," + d.y + ")";
+		return "translate(" + nodes[0].x + "," + nodes[0].y + ")";
 	}).remove();
-}
+
+	var linkUpdate = svg.selectAll(".link").data(links);
+	var linkEnter = linkUpdate.enter();
+	var linkExit = linkUpdate.exit();
+
+	linkEnter.append("path").attr("class", "link").attr("d", d3.svg.diagonal().projection(function(d) {
+		return [nodes[0].x, nodes[0].y]
+	}));
+	linkUpdate.transition().duration(500).attr("d", diagonal);
+	linkExit.transition().duration(500).attr("d", d3.svg.diagonal().projection(function(d) {
+			return [nodes[0].x, nodes[0].y];
+		})).remove();
+	}
