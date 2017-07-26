2bc0862dd3184ce76b368eda87aa8bd3d02bbeb5
Arsenal591 <arsenal591@126.com>
2017-07-26 20:58:21 +0800

    add comments

--- js/visualization.js
+++ js/visualization.js
@@ -1,17 +1,19 @@
-var svg = d3.select("#visualize").append("g").attr("transform", "translate(0,20)");
-var svgTree = d3.layout.tree().separation(function(a, b) {
-	return (a.parent === b.parent ? 1 : 2);
-});
+var svg = d3.select("#visualize").append("g")
+			.attr("transform", "translate(0,20)");
+var svgTree = d3.layout.tree();
+
 var animationTime = 500;
 var currentCenter;
-var data;
-var nodes;
+var data; // cache the data for future use.
+var nodes; // cache the nodes for future use.
 
+//record the offset since last drag-start event.
 var dragdx = 0,
 	dragdy = 0;
 
-function getTreeNodesRecursively(root, level, current, ori) {
-	if (current === level) {
+// from DOM-Nodes to data needed by visualization.
+function getTreeNodesRecursively(root, maxDepth, currentDepth, ori) {
+	if (currentDepth === maxDepth) {
 		return {
 			"children": null,
 			"isme": root === ori,
@@ -24,7 +26,7 @@ function getTreeNodesRecursively(root, level, current, ori) {
 			"node": root
 		}
 		for (let child of root.children) {
-			var x = getTreeNodesRecursively(child, level, current + 1, ori);
+			var x = getTreeNodesRecursively(child, maxDepth, currentDepth + 1, ori);
 			result.children.push(x);
 		}
 		if (root.children.length == 0) {
@@ -34,13 +36,16 @@ function getTreeNodesRecursively(root, level, current, ori) {
 	}
 }
 
-function getTreeNodes(center, level) {
+// center: the DOM-Node you want to focus on
+// maxDepth: how deep the tree is.
+function getTreeNodes(center, maxDepth) {
 	var hasParent = Boolean(center.parent);
 	var root = hasParent ? center.parent : center;
-	var res = getTreeNodesRecursively(root, level, 0, center);
+	var res = getTreeNodesRecursively(root, maxDepth, 0, center);
 	return res;
 }
 
+// fold or unfold the subtree that rooted d. 
 function toggle(d) {
 	if (d.children) {
 		d._children = d.children;
@@ -51,6 +56,7 @@ function toggle(d) {
 	}
 }
 
+//translate all nodes and edges of a subtree.
 function moveSubtree(d, dx, dy) {
 	d.x += dx;
 	d.y += dy;
@@ -67,6 +73,7 @@ function moveSubtree(d, dx, dy) {
 	}
 }
 
+// paint all nodes that similar to d with the same color. 
 function markNodesOfSameType(d, color) {
 	if (d.node.type === "text") {
 		for (let node of nodes) {
@@ -85,6 +92,7 @@ function markNodesOfSameType(d, color) {
 	}
 }
 
+// drag: move subtree.
 function handleDragEvent(d) {
 	var dx = d3.event.dx;
 	var dy = d3.event.dy;
@@ -97,6 +105,7 @@ function handleDragEvent(d) {
 	}
 }
 
+// drag-end: subtree to its original position.
 function handleDragEndEvent(d) {
 	if (dragdx ** 2 + dragdy ** 2 >= 100) {
 		if (d !== nodes[0])
@@ -123,6 +132,7 @@ function handleClickEvent(d) {
 	redraw(currentCenter, true, d);
 }
 
+// scripts to design a context-menu
 contextMenu = function(menu) {
 	d3.selectAll('.d3-context-menu').data([1]).enter()
 		.append('div').attr('class', 'd3-context-menu');
@@ -143,7 +153,6 @@ contextMenu = function(menu) {
 					return d.title;
 				else if(typeof d.title === "function")
 					return d.title(data);
-				return (typeof d.title === 'string') ? d.title : d.title(data);
 			})
 			.on('click', function(d) {
 				d.action(temp, data, index);
@@ -160,6 +169,7 @@ contextMenu = function(menu) {
 	};
 };
 
+// context-menu to be shown when right-clicked.
 var menu = [{
 	title: 'Remove the node.',
 	action: function(elem, d, i) {
@@ -188,12 +198,19 @@ var menu = [{
 	}
 }]
 
-function redraw(center, cached = false, source) {
-	if (!center)
-		center = currentCenter;
+// center: the DOM-Node you want to focus on.
+// cached: true --- means data is not modified, 
+//                  just the structure of the svg-tree is changed.
+//                  (Eg. A subtree is collapsed.)
+//         false --- means data is totally changed
+// source: where entering nodes will come from & exiting nodes go.
+function redraw(center=currentCenter, cached = false, source) {
 	if (!center)
 		return;
 	currentCenter = center;
+
+	// 标记source的原位置，
+	// 这样新节点就会从source的原位置出生，而不是调整后的位置出生
 	var oldSource;
 	if (source)
 		oldSource = {
@@ -221,12 +238,8 @@ function redraw(center, cached = false, source) {
 		d.y = d.depth * 50;
 	})
 
-	if (!source) {
-		source = nodes[0];
-	}
-	if (!oldSource) {
-		oldSource = nodes[0];
-	}
+	source = source || nodes[0];
+	oldSource = oldSource || nodes[0];
 
 	var nodeUpdate = svg.selectAll(".node").data(nodes, function(d) {
 		return d.node.uniqueId;
@@ -255,7 +268,7 @@ function redraw(center, cached = false, source) {
 	enterNodes.call(dragBehavior);
 
 	enterNodes.append("circle")
-		.attr("r", 5)
+		.attr("r", 8)
 		.style("fill", function(d) {
 			if (d.isme)
 				return "red";
@@ -268,7 +281,7 @@ function redraw(center, cached = false, source) {
 			return d.node.type === "element" ? d.node.tagName : d.node.type;
 		})
 		.attr("transform", function(d) {
-			return "translate(" + 5 + "," + 0 + ")";
+			return "translate(5,0)";
 		});
 
 	var updateNodes = nodeUpdate.transition()
@@ -278,7 +291,6 @@ function redraw(center, cached = false, source) {
 		});
 	updateNodes
 		.select("circle")
-		.attr("r", 8)
 		.style("fill", function(d) {
 			if (d.isme)
 				return "red";
